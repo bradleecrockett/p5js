9fa11291ebd89b9db0ae24d177b65f986f36a228
bradleecrockett <bcrockett@rjuhsd.us>
2017-10-12 20:43:45 -0700

    Add Template Files

---
+++ p5jsTemplate/js/index.js
@@ -0,0 +1,20 @@
+//Global Variables
+
+
+//Setup Loop runs once at the beginning of the sketch
+function setup() { 
+	//create the canvas of a particular size
+	var canvas = createCanvas(400, 400);
+	//Give the canvas an id to position it in the HTML
+	canvas.parent('sketch-holder');
+	background(220);
+} 
+
+//Draw loop runs over and over
+function draw() { 
+
+
+}
+
+//Other Function definitions
+
